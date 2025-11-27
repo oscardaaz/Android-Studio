9e733ccf6085f3e28ee4661277c1daa557e11954
Oscar Dominguez Alonso <iamoscaar@gmail.com>
2025-11-27 22:44:48 +0100

    Ejercicio Moviles

--- EjercicioExamen/app/src/main/java/com/example/ejercicioexamen/MainActivity.kt
+++ EjercicioExamen/app/src/main/java/com/example/ejercicioexamen/MainActivity.kt
@@ -4,17 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnCb1 : Button
-    private lateinit var btnCb2 : Button
+    private lateinit var btnCb1 : CheckBox
+    private lateinit var btnCb2 : CheckBox
     private lateinit var btnRB : Button
     private lateinit var rb1 : RadioButton
     private lateinit var rb2 : RadioButton
@@ -22,22 +23,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var spinner : Spinner
     private lateinit var tvMain : TextView
     private lateinit var btnAbrir : Button
+    private lateinit var rGroup : RadioGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnAbrir = findViewById(R.id.btnAbrir)
-
-        btnAbrir.setOnClickListener {
-            Toast.makeText(this,"Pulsado Abrir", Toast.LENGTH_SHORT).show()
-            Log.d("Mi App", "Pulsado Abrir")
-            startActivity(Intent(this, SegundaActivity::class.java))
-        }
+        rGroup = findViewById(R.id.radioGroup)
+        btnCb1 = findViewById(R.id.checkBox1)
+        btnCb2 = findViewById(R.id.checkBox2)
         btnRB = findViewById(R.id.btnRB)
         rb1 = findViewById(R.id.rb1)
         rb2 = findViewById(R.id.rb2)
         rb3 = findViewById(R.id.rb3)
+        spinner = findViewById(R.id.spinner)
         tvMain = findViewById(R.id.tvMain)
         btnRB.setOnClickListener {
             Toast.makeText(this,"Pulsado RB", Toast.LENGTH_SHORT).show()
@@ -53,5 +53,21 @@ class MainActivity : AppCompatActivity() {
             tvMain.text = resultado
         }
 
+
+        configurarBotonAbrir()
+    }
+
+    private fun configurarBotonAbrir() {
+        btnAbrir.setOnClickListener {
+            Toast.makeText(this, "Pulsado Abrir", Toast.LENGTH_SHORT).show()
+            Log.d("Mi App", "Pulsado Abrir")
+
+            val intent = Intent(this, SegundaActivity::class.java)
+            intent.putExtra("CB1", btnCb1.isChecked)
+            intent.putExtra("CB2", btnCb2.isChecked)
+            intent.putExtra("RB", rGroup.checkedRadioButtonId)
+            intent.putExtra("SPINNER", spinner.selectedItemPosition)
+            startActivity(intent)
+        }
     }
 }
--- EjercicioExamen/app/src/main/java/com/example/ejercicioexamen/SegundaActivity.kt
+++ EjercicioExamen/app/src/main/java/com/example/ejercicioexamen/SegundaActivity.kt
@@ -1,20 +1,62 @@
 package com.example.ejercicioexamen
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SegundaActivity : AppCompatActivity() {
+
+    private lateinit var tvUno: TextView
+    private lateinit var tvDos : TextView
+    private lateinit var tvRB : TextView
+    private lateinit var tvSP : TextView
+    private lateinit var btnCambiar : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_segunda)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        tvUno  = findViewById(R.id.tvUno)
+        tvDos  = findViewById(R.id.tvDos)
+        tvRB  = findViewById(R.id.tvRB)
+        tvSP  = findViewById(R.id.tvSpinner)
+        btnCambiar = findViewById(R.id.btnCambiar)
+
+        var cb1 = intent.getBooleanExtra("CB1",false)
+        var cb2 = intent.getBooleanExtra("CB2",false)
+
+        tvUno.text = if (cb1 == true) "Uno: Checked"
+                    else "Uno: Unchecked"
+
+        tvDos.text = if (cb2 == true) "Dos: Checked"
+            else "Dos: Unchecked"
+
+        var rbId = intent.getIntExtra("RB",-1)
+        tvRB.text = when (rbId){
+            R.id.rb1 -> "RB checkeado 1"
+            R.id.rb2 -> "RB checkeado 2"
+            R.id.rb3 -> "RB checkeado 3"
+            else -> ""
+        }
+
+        val spID = intent.getIntExtra("SPINNER",-1)
+        tvSP.text = when (spID){
+            0 -> "Spinner: Uno"
+            1 -> "Spinner: Dos"
+            2 -> "Spinner: Tres"
+            else -> ""
+        }
+
+        btnCambiar.setOnClickListener {
+            val aux = tvUno.text
+            tvUno.text = tvDos.text
+            tvDos.text = aux
         }
     }
 }
