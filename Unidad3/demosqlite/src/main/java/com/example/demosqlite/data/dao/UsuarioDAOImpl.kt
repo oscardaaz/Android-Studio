92d0a4ddc3d690fce6dcd1bacc5a70cdd5d2e568
Oscar Domínguez Alonso <iamoscaar@gmail.com>
2026-01-23 21:12:57 +0100

    ejercicio moviles BBDD

--- Unidad3/demosqlite/src/main/java/com/example/demosqlite/data/dao/UsuarioDAOImpl.kt
+++ Unidad3/demosqlite/src/main/java/com/example/demosqlite/data/dao/UsuarioDAOImpl.kt
@@ -1,4 +1,68 @@
 package com.example.demosqlite.data.dao
 
-class UsuarioDAOImpl {
+import android.content.ContentValues
+import com.example.demosqlite.data.database.UsuariosSQLiteHelper
+import com.example.demosqlite.data.model.Usuario
+
+class UsuarioDAOImpl(
+    private val dbHelper: UsuariosSQLiteHelper
+    ) : UsuarioDAO {
+
+    override fun insertarUsuario(usuario: Usuario): Long {
+
+        val db = dbHelper.writableDatabase
+        val values = ContentValues().apply {
+            // put("nombre" , usuario.nombre)
+            // put("email", usuario.email)
+            put(UsuariosSQLiteHelper.COLUMN_NOMBRE , usuario.nombre)
+            put(UsuariosSQLiteHelper.COLUMN_EMAIL, usuario.email)
+        }
+        val resultado = db.insert(
+            UsuariosSQLiteHelper.TABLE_NAME,
+            null,
+            values
+        )
+
+        db.close()
+        return resultado
+    }
+
+    override fun leerUsuarios(): List<Usuario> {
+        val listaUsuarios = mutableListOf<Usuario>()
+        val db= dbHelper.readableDatabase
+
+        val query = "SELECT * FROM ${UsuariosSQLiteHelper.TABLE_NAME}"
+        // En SQL seria --> SELECT * FROM usuarios
+
+        val cursor = db.rawQuery(query, null)
+
+        if (cursor.moveToNext()){
+            do {
+                val id = cursor.getInt(0)
+                val nombre = cursor.getString(1)
+                val email = cursor.getString(2)
+                listaUsuarios.add(Usuario(id,nombre,email))
+            }while (cursor.moveToNext())
+        }
+
+        cursor.close()
+        db.close()
+        return listaUsuarios
+    }
+
+    override fun leerUsuarioPorId(id: Int): Usuario? {
+        TODO("Not yet implemented")
+    }
+
+    override fun actualizarUsuario(usuario: Usuario): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun borrarUsuario(id: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun borrarTodosLosUsuarios(): Int {
+        TODO("Not yet implemented")
+    }
 }
--- Unidad3/demosqlite/src/main/java/com/example/demosqlite/ui/activities/MainActivity.kt
+++ Unidad3/demosqlite/src/main/java/com/example/demosqlite/ui/activities/MainActivity.kt
@@ -1,21 +1,118 @@
 package com.example.demosqlite.ui.activities
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.demosqlite.R
+import com.example.demosqlite.data.dao.UsuarioDAOImpl
+import com.example.demosqlite.data.database.UsuariosSQLiteHelper
+import com.example.demosqlite.data.model.Usuario
+import com.example.demosqlite.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var operaciones: UsuarioDAOImpl
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Inicializamos el DAO
+        val dbHelper = UsuariosSQLiteHelper(this)
+        operaciones = UsuarioDAOImpl(dbHelper)
+
+        configurarListeners()
+
+        //actualizarListaUsuarios()
+
+    } // Fin del onCreate
+
+
+    private fun configurarListeners() {
+        binding.btnINS.setOnClickListener { manejarInsercion() }
+        binding.btnACT.setOnClickListener { manejarActualizacion() }
+        binding.btnDEL.setOnClickListener { manejarEliminacion() }
+        binding.btnCON.setOnClickListener { manejarConsulta() }
+
     }
-}
+
+    private fun manejarConsulta() {
+        val idTexto = binding.textInputLayoutNombre.editText?.text.toString().trim()
+
+        //CASO 1
+        if (idTexto.isEmpty()) {
+            actualizarListaUsuarios()
+            mostrarMensaje("Mostrando todos los usuarios....")
+            return
+        }
+
+        //CASO 2
+    }
+
+    private fun actualizarListaUsuarios() {
+        val usuarios = operaciones.leerUsuarios()
+        if (usuarios.isEmpty()) {
+            binding.tvOutput.text = "Tabla vacía"
+        } else {
+            val textoTabla = StringBuilder()
+            for (usuario in usuarios){
+                textoTabla.append("$usuario\n")
+            }
+            binding.tvOutput.text = textoTabla.toString()
+        }
+
+
+    }
+
+    private fun manejarEliminacion() {
+        TODO("Not yet implemented")
+    }
+
+    private fun manejarActualizacion() {
+        TODO("Not yet implemented")
+    }
+
+    private fun manejarInsercion() {
+        val nombre = binding.textInputLayoutNombre.editText?.text.toString().trim()
+        val email = binding.textInputLayoutNombre.editText?.text.toString().trim()
+
+        if (nombre.isEmpty() || email.isEmpty()){
+            mostrarMensaje("Completa los campos nombre & email")
+            return
+        }
+        val usuario = Usuario(
+            nombre = nombre,
+            email = email
+        )
+        val idGenerado = operaciones.insertarUsuario(usuario)
+
+        if (idGenerado != -1L) {
+            mostrarMensaje("Usuario insertado con id: $idGenerado")
+            limpiarCampos()
+            //actualizarListaUsuarios();
+        } else {
+            mostrarMensaje("Error al insertar usuario")
+            limpiarCampos()
+        }
+
+    }
+
+    private fun limpiarCampos() {
+        binding.textInputLayoutID.editText?.setText("")
+        binding.textInputLayoutNombre.editText?.setText("")
+        binding.textInputLayoutEmail.editText?.setText("")
+    }
+
+    private fun mostrarMensaje(mensaje: String) {
+        Snackbar.make(
+            binding.root,
+            mensaje,
+            Snackbar.LENGTH_LONG
+        ).show()
+    }
+
+} // Fin de la clase
+
+
+
