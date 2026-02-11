a2a3ae2bdaa29a723867da578e8bb98e71c016d6
Oscar Domínguez Alonso <iamoscaar@gmail.com>
2026-02-11 16:53:37 +0100

    fix funciones

--- ejercicio/app/src/main/java/com/example/ejercicio/ui/activities/MainActivity.kt
+++ ejercicio/app/src/main/java/com/example/ejercicio/ui/activities/MainActivity.kt
@@ -47,6 +47,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun manejarConsulta() {
+        limpiarErrores()
+        limpiarCampos()
+        limpiarFocus()
         actualizarListaReceta()
     }
 
